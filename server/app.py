979eaf04b450cb5c4f7e27c0448909cbc7125423
Allan <allanbarongo5@gmail.com>
2025-06-22 17:03:30 +0300

    Completed lab on authenticating users

--- server/app.py
+++ server/app.py
@@ -47,11 +47,41 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+
+        if not data or 'username' not in data:
+            return make_response({'error': 'Username is required'}, 400)
+
+        user = User.query.filter_by(username=data['username']).first()
+
+        if not user:
+            return make_response({'error': 'User not found'}, 404)
+
+        session['user_id'] = user.id
+        return make_response(jsonify(user.to_dict()), 200)
+    
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict()
+        else:
+            return {}, 401 
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
